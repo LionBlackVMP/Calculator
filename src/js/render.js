29e41fb4b60758d78249b988b80860d9140ac580
LionBlackVMP <Lionblack1079@gmail.com>
2023-10-29 23:30:06 +0100

    fixed bug with input area. now correct shows fractional numbers

--- src/js/render.js
+++ src/js/render.js
@@ -1,10 +1,10 @@
 import { calc } from "./calc.js";
 const btnsArr = ["(", ")", "CE", "×", 7, 8, 9, "÷", 4, 5, 6, "+", 1, 2, 3, "-", 0, "00", ".", "="];
 export const inputValue = { str: "", isResult: false };
-
 if (typeof document !== "undefined") {
   const buttons = document.getElementById("btns-container");
   const result = document.querySelector(".result");
+  const regexpOperators = /["×","÷","\-","+"]/;
 
   btnsArr.forEach((el) => {
     // render all buttons on page
@@ -25,25 +25,45 @@ if (typeof document !== "undefined") {
     if (!e.target.classList.contains("btn")) return;
     const value = e.target.innerHTML;
     inputValue.str = String(inputValue.str);
+    const lastSymbol = inputValue.str[inputValue.str.length - 1];
     if (inputValue.str === "Error") resetCalculator();
     if (isMathSymbol(value)) {
       inputValue.isResult = false;
-      handleMathSymbol(value);
+      handleMathSymbol(value, lastSymbol);
     } else if (value === "CE") {
       deleteLastSymbol();
-    } else if (value === ")" && validBraces(inputValue.str, value)) {
+    } else if (
+      (value === ")" && validBraces(inputValue.str, value)) ||
+      (value === ")" && lastSymbol === ".")
+    ) {
+      return;
+    } else if (value === "(" && lastSymbol === ".") {
       return;
     } else if (value === "=") {
       handleEquals();
+    } else if (value === ".") {
+      canPointExist(value, lastSymbol);
     } else {
       if (inputValue.isResult) {
         inputValue.str = value;
       } else {
         inputValue.str += value;
       }
+
       inputValue.isResult = false;
       result.innerHTML = inputValue.str;
     }
+  };
+
+  const canPointExist = (value, lastSymbol) => {
+    // control if possible to create a fractional number
+    const nums = inputValue.str.split(/["×","÷","\-","+","(",")"]/);
+    const stack = ["", "(", ")"];
+    const lastNumber = nums[nums.length - 1];
+    if (lastNumber.split("").includes(".")) return;
+    if (lastSymbol === ")") return;
+    if (stack.includes(lastNumber)) return;
+    handleMathSymbol(value, lastSymbol);
   };
 
   const deleteLastSymbol = () => {
@@ -59,15 +79,15 @@ if (typeof document !== "undefined") {
 
   const isMathSymbol = (value) => {
     // check if pressed button include math sybmols or point
-    if (value.match(/["×","÷","\-","+","\."]/)) return true;
+    if (value.match(regexpOperators)) return true;
   };
 
-  const handleMathSymbol = (value) => {
+  const handleMathSymbol = (value, lastSymbol) => {
     // control if current negative value have right syntax with brackets
     if (inputValue.str === "") return;
-    const lastSymbol = inputValue.str[inputValue.str.length - 1];
     const penultSymbol = inputValue.str[inputValue.str.length - 2];
-    if (lastSymbol === "(" && isMathSymbol(value) && value !== "-") return; // after open bracket possible to write only minus
+    if (lastSymbol === ".") return;
+    if (lastSymbol === "(") if ((isMathSymbol(value) && value !== "-") || value === ".") return; // after open bracket possible to write only minus
     if (lastSymbol && isMathSymbol(lastSymbol)) {
       if (penultSymbol !== "(") updateMathSymbol(value); // possible to replace math operation
     } else {
